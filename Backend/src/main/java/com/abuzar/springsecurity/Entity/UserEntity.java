b6f15c3061af3a4cb1304e3c37bfa0d0b0e6c83d
Abuzar Khan <abuzarkhan1242@gmail.com>
2025-05-04 15:40:31 +0500

    frontend setup & entites done in backend

---
+++ Backend/src/main/java/com/abuzar/springsecurity/Entity/UserEntity.java
@@ -0,0 +1,38 @@
+package com.abuzar.springsecurity.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "tbl_users")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String verifyOtp;
+    private Boolean isAccountVerified;
+    private Long verifyOtpExpiryAt;
+    private Long resetOtpExpiryAt;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdAt;
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+}
